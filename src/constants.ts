3130bf1e804c66fc88a57d4e5c301243e6b473ca
staka99 <marko.staka12@gmail.com>
2025-04-15 19:50:42 +0200

    Putanje

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const KUPAC_URL = "http://localhost:8080/kupac";
-export const ARTIKL_URL = "http://localhost:8080/artikl";
-export const TRANSAKCIJA_URL = "http://localhost:8080/transakcija";
-export const NALOG_URL = "http://localhost:8080/nalog";
-export const STAVKA_URL = "http://localhost:8080/stavka";
-export const STAVKE_NALOGA_URL = "http://localhost:8080/stavkeZaNalog";
+export const KUPAC_URL = "https://eskladiste-backend.onrender.com/kupac";
+export const ARTIKL_URL = "https://eskladiste-backend.onrender.com/artikl";
+export const TRANSAKCIJA_URL = "https://eskladiste-backend.onrender.com/transakcija";
+export const NALOG_URL = "https://eskladiste-backend.onrender.com/nalog";
+export const STAVKA_URL = "https://eskladiste-backend.onrender.com/stavka";
+export const STAVKE_NALOGA_URL = "https://eskladiste-backend.onrender.com/stavkeZaNalog";
